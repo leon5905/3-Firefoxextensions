fedc9f1226c07c69145b3b29a2aed60290b0bba4
yeohw <yeohweizhu@gmail.com>
2017-10-25 00:00:46 +0800

    Added open tab functionality + Reorganized workspace

---
+++ script/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#btn-open-seperate").click(function(){
+        var win = window.open('/webpage/browser-action-popup.html');
+        if (win) {
+            //Browser has allowed it to be opened
+            win.focus(); //Focus on the new web page
+            window.close(); //Close the current one
+        } else {
+            //Browser has blocked it
+            alert('Please allow popups for this website');
+        }
+    });
+});
