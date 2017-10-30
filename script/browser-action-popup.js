0acea3e4b063125ead4868a3b0da2320cd896b82
yeohw <yeohweizhu@gmail.com>
2017-10-30 21:50:09 +0800

    Added storage sync with new account page

--- script/browser-action-popup.js
+++ script/browser-action-popup.js
@@ -29,11 +29,18 @@ $(document).ready(function(){
     //Assign Function to Home
 
     //Assign Function to Settings
+
+    // browser.storage.sync.clear(); //Purge Storage
+
+    console.log(mainWindow.recordList.length);
 });
 
+//Collection of global var
 var mainWindow = {
     pushNotification: function (messageString, notificationType, seconds){
-        
+        //Z-index 1000
+
+
     },
 
     getURL: function(callback){
@@ -41,7 +48,26 @@ var mainWindow = {
         getting.then(callback, null);
     },
 
+    saveData:function(url,name,username,password,note){
+        //TODO save data
+        console.log(password);
+        var encrypted = CryptoJS.AES.encrypt(password, this.masterKey);
+        console.log(encrypted.ciphertext);
+        var decrypted = CryptoJS.AES.decrypt(encrypted, this.masterKey);
+        console.log(decrypted.toString(CryptoJS.enc.Utf8));
+
+        var obj = {url,name,username,password,note}
+        this.recordList[this.recordList.length] = obj;
+
+        browser.storage.sync.set({
+            recordList:  mainWindow.recordList
+          });
+
+    },
+
     addNewItemWindow:null,
+
+    //Colors
     colorPrimary1: "#555",
     colorGreen: "#007000",
     colorGreenLight: "#008000",
@@ -49,10 +75,64 @@ var mainWindow = {
     colorBlackWhiten:"#202020",
     colorSectionLineGrey:"#ddd",
 
-    fontSizeWindowTitle: '16px'
-}
+    //Font Size
+    fontSizeWindowTitle: '16px',
 
+    //Master Key
+    masterKey :"masterkey",
+
+    //All of the record
+    recordList: [],
+
+    // //IsSync
+    // isSync:true
+};
+
+//Load storage area
+var ywzPMStorage = browser.storage.sync.get(
+    {
+        recordList:[]
+    }
+);
+ywzPMStorage.then(function(item){
+    console.log(item);
+    if (item){
+        mainWindow.recordList = item.recordList;
+        // for (var i=0;i<item.length;i++){
+        //     item[i]
+        // }
+
+        console.log(mainWindow.recordList.length)
+        console.log(mainWindow.recordList)
+    }
+},
+function(error){
+    console.log(error)
+});
+
+//Main Page Section
+var mainPageHelper = {
+    loadMainPage:function(recordList){ //Load the main page by ini / reinitialize the main page content
+        
+    },
+
+    searchFunction: function(parameter){ // Will search for match in Name
+        //Go through list, disable and enable visibility
+        
+    }
+};
+
+//Add New Item Section
 var addNewItemHelper = {
+    //cache of id
+    urlInputId:"URL",
+    nameInputId:"NameInput",
+    usernameInputId:"UserNameInput",
+    passwordInputId:"PasswordInput",
+    noteInputId:"NoteInput",
+    divId:"FormInput",
+
+    //Generate new item section
     addNewItem: function (parent){
         //Add New Item Page
         //Slide up animation to cover the parent
@@ -62,16 +142,12 @@ var addNewItemHelper = {
         addNewItemDiv.attr('id',addNewItemDivId);
         addNewItemDiv.css('position','absolute');
         addNewItemDiv.css('width','100%');
+        addNewItemDiv.css('height','100%');
         addNewItemDiv.css('background-color','gainsboro');
         addNewItemDiv.css('z-index','999');
         addNewItemDiv.css("top","100%");
         addNewItemDiv.css("transition","all .4s ease-out");
         //addNewItemDiv.css();
-        
-        //
-        var addNewItemForm=$('<form></form>')//Create top-level form
-    
-        addNewItemDiv.append(addNewItemForm);
     
         //
         var addNewItemHeader=$('<div></div>'); //Create Header
@@ -79,7 +155,7 @@ var addNewItemHelper = {
         addNewItemHeader.css('width',"100%");
         addNewItemHeader.css('background-color',"#555");
         addNewItemHeader.css('color',"white");
-        addNewItemForm.append(addNewItemHeader);
+        addNewItemDiv.append(addNewItemHeader);
     
         var addNewItemCancel=$("<div></div>"); //Create Cancel Section
         addNewItemCancel.click(function () {
@@ -127,7 +203,12 @@ var addNewItemHelper = {
     
         var addNewSave=$('<div></div>');
         addNewSave.click(function () {
+            var targetDiv = $('#'+addNewItemHelper.divId);
+
+            mainWindow.saveData(targetDiv.find('#'+addNewItemHelper.urlInputId).val(),targetDiv.find('#'+addNewItemHelper.nameInputId).val(),targetDiv.find('#'+addNewItemHelper.usernameInputId).val(),
+            targetDiv.find('#'+addNewItemHelper.passwordInputId).val(), targetDiv.find('#'+addNewItemHelper.noteInputId).val());
             mainWindow.addNewItemWindow.css('top','100%');
+
             setTimeout(function(){
                 mainWindow.addNewItemWindow.remove();
                 mainWindow.addNewItemWindow = null;
@@ -162,9 +243,11 @@ var addNewItemHelper = {
         //
         //Craete Content (Body)
         var addNewItemContent=$('<div></div>')
-        addNewItemContent.css('height',"1000px");
+        addNewItemContent.css('height',"calc(100% - 45px)");
         addNewItemContent.css('top',"45px");
-        addNewItemForm.append(addNewItemContent);
+        addNewItemContent.css('overflow',"auto");
+        addNewItemContent.attr('id',this.divId);
+        addNewItemDiv.append(addNewItemContent);
     
         var addNewItemForm=$('<div></div>'); //Top-Level form div
         addNewItemContent.append(addNewItemForm);
@@ -178,7 +261,7 @@ var addNewItemHelper = {
         addNewItemInfoSection.append(addNewItemInfoList);
 
         var addNewItemInfoURL = this.generateContentSectionStandardDiv("URL"); //URL of the record
-        var addNewItemInfoURLInput = this.generateContentSectionInputField('URL','text');
+        var addNewItemInfoURLInput = this.generateContentSectionInputField(this.urlInputId,'text');
         
         mainWindow.getURL(function(windowInfo){
             tabInfo = windowInfo.tabs;
@@ -194,16 +277,16 @@ var addNewItemHelper = {
         addNewItemInfoList.append(addNewItemInfoURL);
 
         var addNewItemInfoName = this.generateContentSectionStandardDiv("Name"); //Name of the record
-        var addNewItemInfoNameInput = this.generateContentSectionInputField('NameInput','text');
+        var addNewItemInfoNameInput = this.generateContentSectionInputField(this.nameInputId,'text');
         addNewItemInfoName.append(addNewItemInfoNameInput);
         addNewItemInfoList.append(addNewItemInfoName);
 
         var addNewItemInfoUserName = this.generateContentSectionStandardDiv('Username')//UserName
-        addNewItemInfoUserName.append(this.generateContentSectionInputField('UserNameInput','text'));
+        addNewItemInfoUserName.append(this.generateContentSectionInputField(this.usernameInputId,'text'));
         addNewItemInfoList.append(addNewItemInfoUserName);
 
         var addNewPassword = this.generateContentSectionStandardDiv("Password"); //Password
-        var addNewPasswordInputSpan = this.generateContentSectionInputField('PasswordInput','password');
+        var addNewPasswordInputSpan = this.generateContentSectionInputField(this.passwordInputId,'password');
         addNewPassword.append(addNewPasswordInputSpan);
         addNewPasswordInputSpan.append(this.generateContentSectionViewPassowrd(addNewPasswordInputSpan.find("input")));
         addNewItemInfoList.append(addNewPassword);
@@ -211,24 +294,36 @@ var addNewItemHelper = {
         var addNewPasswordGenerator = this.generatePasswordGenerator(); //Password Generator
         addNewItemInfoList.append(addNewPasswordGenerator);
 
-        /*End of Info Fild*/
+        /*End of Info Field*/
 
-        var addNewItemCustomSection=$('<div></div');//Section Custom Field
-        addNewItemCustomSection.addClass('popup-list-section');
-        addNewItemCustomSection.append(this.generateContentSectionHeader("Custom Field"));
-    
+        // var addNewItemCustomSection=$('<div></div');//Section Custom Field
+        // addNewItemCustomSection.addClass('popup-list-section');
+        // addNewItemCustomSection.append(this.generateContentSectionHeader("Custom Field"));
+
         
         /*End of Custoim Field */
 
         var addNewItemNoteSection=$('<div></div');//Section Note
         addNewItemNoteSection.addClass('popup-list-section');
         addNewItemNoteSection.append(this.generateContentSectionHeader("Note"));
-    
+
+        var addNewItemNoteList=$('<div></div>');
+        addNewItemNoteList.css('border-top','2px solid lightgrey');
+        addNewItemNoteList.css('border-bottom','2px solid lightgrey');
+        addNewItemInfoSection.append(addNewItemNoteList);
+
+        var addNewItemNoteDiv = this.generateContentSectionStandardDiv("Note Area");
+        addNewItemNoteDiv.append(this.generateTextArea(this.noteInputId));
+        addNewItemNoteSection.append(addNewItemNoteDiv);
+
+
+        /*End of Note Section */
+
         addNewItemForm.append(addNewItemInfoSection);
-        addNewItemForm.append(addNewItemCustomSection);    
+        // addNewItemForm.append(addNewItemCustomSection);    
         addNewItemForm.append(addNewItemNoteSection);
         
-        parent.append(addNewItemDiv); //Add generate tab to parent
+        parent.append(addNewItemDiv); //Add generated tab to parent
     
         mainWindow.addNewItemWindow = addNewItemDiv; //Cache the created window
     
@@ -279,6 +374,24 @@ var addNewItemHelper = {
         var returnResult = $('<input></input>');
         returnResult.attr('id',inputID);
         returnResult.attr('type',type);
+        returnResult.addClass('content-focus');
+        returnResult.css('width','100%');
+        returnResult.css('border','none');
+        returnResult.css('background-color','transparent');
+
+        spanReturnResult.css('border-bottom','1px solid #E8E8E8');
+
+        spanReturnResult.append(returnResult);
+
+        return spanReturnResult;
+    },
+
+    generateTextArea:function(inputID){
+        var spanReturnResult = $('<span></span>');
+        spanReturnResult.css('display','flex');
+
+        var returnResult = $('<textarea></textarea>');
+        returnResult.attr('id',inputID);
         returnResult.addClass('content-focus');
         returnResult.css('width','100%');
         returnResult.css('border','none');
@@ -349,7 +462,6 @@ var addNewItemHelper = {
 
         var label = $('<label>' + labelName + '</label>');
         label.css('color','#777777');
-        // label.css('margin-bottom','5px');
         label.css('font-size','13px');
         label.css('display','block');
 
